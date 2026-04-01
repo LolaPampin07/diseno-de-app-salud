08e5731e889de0c031d93a4135038d31e3e1e6b5
Lola Pampin <ljpampin@estudiantes.unsam.edu.ar>
2026-04-01 20:35:20 -0300

    clase 01-04

---
+++ app_nueva/@latest/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="FamilyMed"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+      q-button label="Nueva reunión" @click="alert('Ingrese su horario dispobible')"
+    >
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
